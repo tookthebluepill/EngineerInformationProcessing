9fca45733e0660d873f9b3152622105b22056705
tookthebluepill <jameslee0131@gmail.com>
2025-07-02 11:23:28 +0900

    25년도 1회차

---
+++ 25년도 1회차/JAVA/13.java
@@ -0,0 +1,39 @@
+// 13. 다음은 Java 코드에 대한 문제이다. 아래 코드를 확인하여 알맞는 출력값을 작성하시오.
+
+public class Main {
+    public static void main(String[] args) {
+        new Child();
+        System.out.println(Parent.total);
+    }
+}
+ 
+ 
+class Parent {
+    static int total = 0;
+    int v = 1;
+ 
+    public Parent() {
+        total += (++v);
+        show();    
+    }
+ 
+    public void show() {
+        total += total;
+    }
+}
+ 
+ 
+class Child extends Parent {
+    int v = 10;
+ 
+    public Child() {
+        v += 2;
+        total += v++;
+        show();
+    }
+ 
+    @Override
+    public void show() {
+        total += total * 2;
+    }
+}
